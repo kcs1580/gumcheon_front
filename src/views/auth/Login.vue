437b27e351bb85656be3cf60e89259d47b7ceeb5
김창수 <kcs1580@naver.com>
2021-12-15 10:57:34 +0900

    로그인 화면 여백 수정

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,194 +1,204 @@
 <template>
-  <!-- 로그인 페이지 -->  
-  <div style="text-align:center">
-        <v-dialog
-      v-model="dialog"
-      max-width="321"
-    >
+  <!-- 로그인 페이지 -->
+  <div style="text-align: center">
+    <v-dialog v-model="dialog" max-width="321">
       <template v-slot:activator="{ on, attrs }">
-       
-  <v-card flat width="375px" ><button text>
-     <div style="width:239px; height:50px; display:inline-block" >
-       
-    <v-img @click="gotoUrl('/')"        
-              src="@/assets/img/gfmc_ci_gray.png" 
-            />
-   
-     </div></button>
-       
-     
-      <div  @keydown.enter="login(id, pw)">
-        <div style="width:265px;margin-top:39px; margin-bottom:39px;display:inline-block">
-          <v-text-field style="margin:0px;padding:0px" height="49px"          
-            placeholder="아이디를 입력해주세요"           
-            hide-details           
-            v-model="id"
-            flat
-          />
-          <v-text-field style="margin:0px;padding:0px"
+        <v-card
           flat
-            clearable height="49px"
-            hide-details
-            placeholder="비밀번호를 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
+          width="375px"
+          style="padding-top: 81px; padding-bottom: 15px"
+          ><button text>
+            <div style="width: 239px; height: 50px; display: inline-block">
+              <v-img
+                @click="gotoUrl('/')"
+                src="@/assets/img/gfmc_ci_gray.png"
+              />
+            </div>
+          </button>
 
-      
-        </div>
-        
+          <div @keydown.enter="login(id, pw)">
+            <div
+              style="
+                width: 265px;
+                margin-top: 39px;
+                margin-bottom: 39px;
+                display: inline-block;
+              "
+            >
+              <v-text-field
+                style="margin: 0px; padding: 0px"
+                height="49px"
+                placeholder="아이디를 입력해주세요"
+                hide-details
+                v-model="id"
+                flat
+              />
+              <v-text-field
+                style="margin: 0px; padding: 0px"
+                flat
+                clearable
+                height="49px"
+                hide-details
+                placeholder="비밀번호를 입력해주세요"
+                v-model="pw"
+                type="password"
+              />
+            </div>
 
-       
-          <v-btn
-        @click="login(id, pw)"
-       rounded
-        height="52"
-        width="291"
-        color="#0276F9
+            <v-btn
+              @click="login(id, pw)"
+              rounded
+              height="52"
+              width="291"
+              color="#0276F9
 "
-        ><div class="login-btn white--text">로그인</div></v-btn
-      >           <div style="display:inline-block; margin-top:12px">
-           
-              <a class="find-btn" @click="undone()" 
-                >아이디 찾기</a
-              >|<a class="find-btn" @click="undone()"  
+              ><div class="login-btn white--text">로그인</div></v-btn
+            >
+            <div style="display: inline-block; margin-top: 12px">
+              <a class="find-btn" @click="undone()">아이디 찾기</a>|<a
+                class="find-btn"
+                @click="undone()"
                 >비밀번호 찾기</a
               >
-     
-       </div>
+            </div>
 
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="signup(id, user_nm, pw), (dialog = false)"
+              rounded
+              height="52"
+              width="291"
+              color="#0276F9
+"
+              outlined
+              style="margin-top: 72px"
+              ><div class="login-btn">비밀번호 초기화</div></v-btn
+            >
+          </div>
+          <footerbar />
+        </v-card>
+      </template>
+      <v-card rounded="xl" width="321px">
+        <v-row no-gutters align="center" justify="center">
+          <div style="width: 261px; display: inline-block; margin-top: 23px">
+            <v-text-field
+              height="34px"
+              placeholder="아이디를 입력해주세요"
+              hide-details
+              v-model="id"
+            />
+            <v-text-field
+              clearable
+              height="34px"
+              hide-details
+              placeholder="소속부서를 입력해주세요"
+              v-model="pw"
+              type="password"
+            />
+            <v-text-field
+              clearable
+              height="34px"
+              hide-details
+              placeholder="소속팀을 입력해주세요"
+              v-model="pw"
+              type="password"
+            />
+            <v-text-field
+              clearable
+              height="34px"
+              hide-details
+              placeholder="직통 전화번호를 입력해주세요"
+              v-model="pw"
+              type="password"
+            />
+          </div>
+          <div style="width: 273px; margin-top: 24px" class="reset-detail-box">
+            시스템관리자에게 비밀번호 초기화를 요청합니다.<br />
+            초기 비밀번호를 안내받으신 후 로그인하여 주시기 바랍니다.
+          </div>
 
-       
-             <v-btn v-bind="attrs"  v-on="on" 
-        @click="signup(id,user_nm, pw),dialog = false"
-       rounded
-        height="52"
-        width="291"
-        color="#0276F9
-" outlined style="margin-top:72px"
-        ><div   
-         class="login-btn">비밀번호 초기화</div></v-btn
-      >
+          <!-- 하단 버튼 -->
 
-    
-         
-      </div>
-              <footerbar/>
-
+          <v-btn
+            style="margin-top: 26px; margin-bottom: 29px"
+            @click="signup(id, user_nm, pw)"
+            rounded
+            height="49"
+            width="266"
+            color="#0276F9"
+            ><div
+              v-bind="attrs"
+              v-on="on"
+              class="login-btn white--text"
+              @click="dialog = false"
+            >
+              비밀번호 초기화 신청
+            </div></v-btn
+          >
+        </v-row>
       </v-card>
-      </template>
-     <v-card rounded="xl" width="321px" > 
-       <v-row no-gutters align="center" justify="center" >
-        <div style="width:261px;display:inline-block;margin-top:23px;">
-          <v-text-field height="34px"          
-            placeholder="아이디를 입력해주세요"           
-            hide-details           
-            v-model="id"
-          />
-          <v-text-field
-            clearable height="34px"
-            hide-details
-            placeholder="소속부서를 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
-  <v-text-field
-            clearable height="34px"
-            hide-details
-            placeholder="소속팀을 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
-  <v-text-field
-            clearable height="34px"
-            hide-details
-            placeholder="직통 전화번호를 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
-      
-      
-        </div>
-        <div style="width:273px; margin-top:24px;" class="reset-detail-box">          
-         시스템관리자에게 비밀번호 초기화를 요청합니다.<br/>    
-      초기 비밀번호를 안내받으신 후 로그인하여 주시기 바랍니다.
-      </div>
-    
-
-     <!-- 하단 버튼 -->
- 
- <v-btn style="margin-top:26px;margin-bottom:29px"
-        @click="signup(id,user_nm, pw)"
-       rounded
-        height="49"
-        width="266"
-        color="#0276F9"
-        ><div   v-bind="attrs"
-          v-on="on" class="login-btn white--text" @click="dialog = false">비밀번호 초기화 신청</div></v-btn
-      >       
-          </v-row> </v-card>
     </v-dialog>
-      
 
-       <v-dialog
-      v-model="dialog2"
-      max-width="321"
-    >
+    <v-dialog v-model="dialog2" max-width="321">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-비밀번호 재설정        </v-btn>
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+          비밀번호 재설정
+        </v-btn>
       </template>
-     <v-card rounded="xl" width="321px" > 
-       <v-row no-gutters align="center" justify="center" >
-        <div style="width:261px;display:inline-block;margin-top:23px;">
-          <v-text-field height="34px"          
-            placeholder="기존 비밀번호를 입력해주세요"           
-            hide-details           
-            v-model="id"
-          />
-          <v-text-field
-            clearable height="34px"
-            hide-details
-            placeholder="변경할 비밀번호를 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
-  <v-text-field
-            clearable height="34px"
-            hide-details
-            placeholder="변경할 비밀번호를 한번 더 입력해주세요"           
-            v-model="pw"
-            type="password"
-          />
-      
-      
-        </div>
-        <div style="width:273px; margin-top:24px;" class="reset-detail-box">          
-         비밀번호는 8자리 이상 숫자, 특수문자를<br/>    
-반드시 포함하여야 합니다.</div>
-    
+      <v-card rounded="xl" width="321px">
+        <v-row no-gutters align="center" justify="center">
+          <div style="width: 261px; display: inline-block; margin-top: 23px">
+            <v-text-field
+              height="34px"
+              placeholder="기존 비밀번호를 입력해주세요"
+              hide-details
+              v-model="id"
+            />
+            <v-text-field
+              clearable
+              height="34px"
+              hide-details
+              placeholder="변경할 비밀번호를 입력해주세요"
+              v-model="pw"
+              type="password"
+            />
+            <v-text-field
+              clearable
+              height="34px"
+              hide-details
+              placeholder="변경할 비밀번호를 한번 더 입력해주세요"
+              v-model="pw"
+              type="password"
+            />
+          </div>
+          <div style="width: 273px; margin-top: 24px" class="reset-detail-box">
+            비밀번호는 8자리 이상 숫자, 특수문자를<br />
+            반드시 포함하여야 합니다.
+          </div>
 
-     <!-- 하단 버튼 -->
- 
- <v-btn style="margin-top:26px;margin-bottom:29px"
-        @click="signup(id,user_nm, pw)"
-       rounded
-        height="49"
-        width="266"
-        color="#0276F9"
-        ><div   v-bind="attrs"
-          v-on="on" class="login-btn white--text" @click="dialog = false">비밀번호 변경</div></v-btn
-      >       
-          </v-row> </v-card>
+          <!-- 하단 버튼 -->
+
+          <v-btn
+            style="margin-top: 26px; margin-bottom: 29px"
+            @click="signup(id, user_nm, pw)"
+            rounded
+            height="49"
+            width="266"
+            color="#0276F9"
+            ><div
+              v-bind="attrs"
+              v-on="on"
+              class="login-btn white--text"
+              @click="dialog = false"
+            >
+              비밀번호 변경
+            </div></v-btn
+          >
+        </v-row>
+      </v-card>
     </v-dialog>
-
   </div>
-
 </template>
 
 <script>
@@ -196,21 +206,21 @@ import { headerbar, footerbar } from "@/views/user";
 
 import userApi from "@/api/user.js";
 export default {
-    components: { headerbar, footerbar },
+  components: { headerbar, footerbar },
 
   data() {
     return {
-      user:{
-         id: "",
-      pw: "",
-      user_nm: "",
+      user: {
+        id: "",
+        pw: "",
+        user_nm: "",
       },
       id: "",
-      pw: "",    
-      user_nm: "",  
+      pw: "",
+      user_nm: "",
       msg: "",
-       dialog: false,dialog2: false,
-
+      dialog: false,
+      dialog2: false,
     };
   },
   methods: {
@@ -227,32 +237,31 @@ export default {
       // LOGIN 액션 실행
       this.$store
         .dispatch("AUTH_REQUEST", { id, pw })
-        .then(res => {
-       this.$router.go(-1);
+        .then((res) => {
+          this.$router.go(-1);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
     },
-    signup(id, user_nm,pw) {
-
-this.user.id = id;
-this.user.user_nm = user_nm;
-this.user.pw = pw;
-      this.loading = true
+    signup(id, user_nm, pw) {
+      this.user.id = id;
+      this.user.user_nm = user_nm;
+      this.user.pw = pw;
+      this.loading = true;
 
       userApi
         .signup(this.user)
-        .then(res => {
-         console.log(res);
+        .then((res) => {
+          console.log(res);
           this.loading = false;
-this.$router.push("/login")
+          this.$router.push("/login");
         })
-        .catch(res => {
+        .catch((res) => {
           console.log(res);
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -274,25 +283,24 @@ this.$router.push("/login")
 .login-btn {
   font-size: 18px;
 }
-.v-btn{
+.v-btn {
   border-radius: 12px;
 }
 
-.reset-detail-box{
+.reset-detail-box {
   font-size: 11px;
   width: 273px;
   height: 34px;
   text-align: center;
   margin: 0 auto;
 }
-.v-btn:hover :before  {
-background: #55A5FF;}
-.find-btn{
+.v-btn:hover :before {
+  background: #55a5ff;
+}
+.find-btn {
   font-size: 13px;
   font-style: none;
-color: #313640;
-margin: 14px;
+  color: #313640;
+  margin: 14px;
 }
-
-
 </style>
--- src/views/user/nav/footer.vue
+++ src/views/user/nav/footer.vue
@@ -1,29 +1,30 @@
 <template>
   <!-- footer -->
   <v-footer color="white" class="footer-box">
-    
     <v-row no-gutters justify="space-between" align="end">
       <v-col cols="6">
-     
-  <p class="text-left">
-금천구시설관리공단<br/>
-사업자등록번호 : 119-82-03909<br/>
-대표 : 박 평<br/>
-08638 서울시 금천구 시흥대로 37길 18 (시흥동)<br/>
-전화 02.807.0061~7<br/>
-팩스 02.809.1471~2<br/>
-</p>
+        <p class="text-left">
+          금천구시설관리공단<br />
+          사업자등록번호 : 119-82-03909<br />
+          대표 : 박 평<br />
+          08638 서울시 금천구 시흥대로 37길 18 (시흥동)<br />
+          전화 02.807.0061~7<br />
+          팩스 02.809.1471~2<br />
+        </p>
       </v-col>
       <v-col cols="6">
-        <p class="text-right" >
-      <a>이용약관</a> <br/>
-      <a>개인정보처리</a> <br/>
-      <a>고객센터</a> <br/>    
-    </p>
+        <p class="text-right">
+          <a>이용약관</a> <br />
+          <a>개인정보처리</a> <br />
+          <a>고객센터</a> <br />
+        </p>
       </v-col>
       <v-col cols="12">
-       <p class="text-left"> Copyright 2021  서울시금천구시설관리공단  All Rights Reserved </p> </v-col>
-      </v-row>
+        <p class="text-left">
+          Copyright 2021 서울시금천구시설관리공단 All Rights Reserved
+        </p>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
@@ -32,40 +33,34 @@ export default {
   methods: {
     preparing() {
       alert("구현중입니다.");
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-.footer-box{
-
-  margin-top:100px;
+.footer-box {
+  margin-top: 100px;
   font-size: 10px;
 }
 .re_footer .policyWrap {
-    height: auto;
-    text-align: center;
-    padding: 10px 0;
+  height: auto;
+  text-align: center;
+  padding: 10px 0;
 }
 .re_footer .policyWrap .policy {
-    width: 100%;
-    box-sizing: border-box;
-    padding: 0 5px;
-    
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 5px;
 }
-.re_footer .policyWrap .policy li a{
-    
-
- text-decoration: none;
-    color: #636363;
+.re_footer .policyWrap .policy li a {
+  text-decoration: none;
+  color: #636363;
 }
 
 .re_footer .policyWrap .policy li {
   float: left;
- list-style-type: none;
-margin-left: 20px;
+  list-style-type: none;
+  margin-left: 20px;
 }
-
-
 </style>
